96c3973e66b59f781cf083b2408d0635ca9c3e95
Petrii <petri.prk@gmail.com>
2014-12-10 14:00:34 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,20 +1,18 @@
-<?php
-//READ CompanyList.csv / users.csv
 function readList($filename){
 	//initialize variables
 	$list = array();
 	$i=0;
 	
 	if (file_exists($filename)) {
-		$fileOpenCSV = fopen($filename, 'r') or die ('Epic fail ( $filename );-P');
+		$fileOpenCSV = fopen($filename, 'r') or die ('Epic fail "'.$filename.'";-P');
 		while(! feof($fileOpenCSV)){
 			$list[$i] = fgetcsv($fileOpenCSV);
 			$i++;
 		}
 		fclose($fileOpenCSV);
 	} else {
-		echo "The file $filename does not exist";
+		//echo "The file $filename does not exist";
+		return $error = "The file $filename does not exist";
 	}
 	return $list;
 }
-?>
